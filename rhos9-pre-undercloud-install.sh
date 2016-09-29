fa25b008db8ef214d0a67148dbb72b0f9f924535
Florian Fuchs <flo.fuchs@gmail.com>
2016-09-29 14:50:35 -0400

    Pin rhos-release 10 to specific puddle; Use tripleo-common 5.1.0 as base

--- rhos9-pre-undercloud-install.sh
+++ rhos9-pre-undercloud-install.sh
@@ -6,20 +6,17 @@ set -e
 # Install the latest puddle
 sudo yum localinstall -y http://rhos-release.virt.bos.redhat.com/repos/rhos-release/rhos-release-latest.noarch.rpm
 
-# Install python-cryptography from 9, since it's not included in 10
-sudo rhos-release 9
-sudo yum install -y python-cryptography
-
 # Install mistral packages from rhos10
-sudo rhos-release 10
+sudo rhos-release 10 -p 2016-09-07.6
 sudo yum install -y openstack-mistral-all
 sudo yum install -y openstack-mistral-api
 sudo yum install -y openstack-mistral-engine
 sudo yum install -y openstack-mistral-executor
 
 # Switch pack to 9-director and remove rhos10 repo
-sudo rhos-release -P 9-director
 sudo rm /etc/yum.repos.d/rhos-release-10.repo
+sudo yum clean all
+sudo rhos-release -P 9-director
 
 # Install dependencies for ansible installation
 sudo yum install -y gcc
